a337947f425ad4aa2aa1f0a64dd40dca349e7487
Darwin <163784163+D4rwinT@users.noreply.github.com>
2025-11-29 22:02:25 -0300

    Fix typo
    Corrected a typo in the greeting message for Chile.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/chile', (req, res) => {
-  res.send('Un slaudo a todos los chilenos que aportan a esta teletón.');
+  res.send('Un salaudo a todos los chilenos que aportan a esta teletón.');
 });
 
 app.listen(port, () => {
