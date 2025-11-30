bb61acf2e608ba8d2f709f4c3657434680f713ec
Darwin <163784163+D4rwinT@users.noreply.github.com>
2025-11-29 22:01:31 -0300

    Add new route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hola! Esta es la versión 1.1 de mi aplicación en Azure para la prueba 2 de DevOps.');
 });
 
+app.get('/chile', (req, res) => {
+  res.send('Un slaudo a todos los chilenos que aportan a esta teletón.');
+});
+
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
 });
